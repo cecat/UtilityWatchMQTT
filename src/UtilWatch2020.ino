8a5f1e1856b28f7d555e2c52591e18b7576a3847
CeC <cecatlett@gmail.com>
2020-10-12 23:17:22 -0500

    make it easy to comment out ThingSpeak reporting

--- src/UtilWatch2020.ino
+++ src/UtilWatch2020.ino
@@ -159,41 +159,9 @@ void setup() {
 void loop() {
 
     if ((millis() - lastReport) > reportFreq) {         // time to report yet? 
-    
-    // Report to Particle cloud --> webhooks --> ThingSpeak
-        int cases = 5;                                  // hard code case # here, feels like a kluge
-        switch ((reportCount++) % cases) {              // round robin reporting one var at a time
-            case 0:                                     // sump current
-                for (int j=0;j<HIST;j++) {              // report the max we've seen in case we missed a short run
-                    sumpCur = max (sumpCur, sumpHistory[j]);
-                    sumpHistory[j]=0;
-                }
-                Particle.publish("sumpCurrent", String(sumpCur),    PRIVATE);
-                break;
-            case 1:                                     // hvac current
-                Particle.publish("hvacCurrent", String(hvacCur),    PRIVATE);
-                break;
-            case 2:                                     // water heater chimney temperature
-                Particle.publish("waterTemp",   String(waterTemp),  PRIVATE);
-                break;
-            case 3:                                     // duration of last hvac event (if there was one; 0 if not)
-                if (hvacEvent) {
-                    if (!hvacOn) Particle.publish("hvacEvent", String(hvacDuration), PRIVATE);
-                    hvacEvent = false;
-                } else {
-                    Particle.publish("hvacEvent", "0", PRIVATE);
-                }
-                break;
-            case 4:                                     // duration of last sump event (if there was one; 0 if not)
-                if (sumpEvent) {
-                    Particle.publish("sumpEvent", String(sumpDuration), PRIVATE);
-                    sumpEvent = false;
-                } else {
-                    Particle.publish("sumpEvent", "0", PRIVATE);
-                }
-                break;
-        }
-            
+    // Report to ThingSpeak
+        ThingSpeakReport();
+
     // Report to HA via MQTT
         if (client.isConnected()) {
 
@@ -343,4 +311,46 @@ double getTemp() {  // using example code from the DS18B20 library
     Serial.println("Invalid reading");
   }
   return (fahrenheit); // F, because this is Amurica
+}
+
+/*
+ * This code is useful if one wants to use webhooks at the Particle Cloud
+ * to send data to ThingSpeak.com - it predates the Home Assistant setup but I've
+ * kept it as it now has several years of historical data about my appliance behavior
+ */
+void ThingSpeakReport() {
+    // Report to Particle cloud --> webhooks --> ThingSpeak
+        int cases = 5;                                  // hard code case # here, feels like a kluge
+        switch ((reportCount++) % cases) {              // round robin reporting one var at a time
+            case 0:                                     // sump current
+                for (int j=0;j<HIST;j++) {              // report the max we've seen in case we missed a short run
+                    sumpCur = max (sumpCur, sumpHistory[j]);
+                    sumpHistory[j]=0;
+                }
+                Particle.publish("sumpCurrent", String(sumpCur),    PRIVATE);
+                break;
+            case 1:                                     // hvac current
+                Particle.publish("hvacCurrent", String(hvacCur),    PRIVATE);
+                break;
+            case 2:                                     // water heater chimney temperature
+                Particle.publish("waterTemp",   String(waterTemp),  PRIVATE);
+                break;
+            case 3:                                     // duration of last hvac event (if there was one; 0 if not)
+                if (hvacEvent) {
+                    if (!hvacOn) Particle.publish("hvacEvent", String(hvacDuration), PRIVATE);
+                    hvacEvent = false;
+                } else {
+                    Particle.publish("hvacEvent", "0", PRIVATE);
+                }
+                break;
+            case 4:                                     // duration of last sump event (if there was one; 0 if not)
+                if (sumpEvent) {
+                    Particle.publish("sumpEvent", String(sumpDuration), PRIVATE);
+                    sumpEvent = false;
+                } else {
+                    Particle.publish("sumpEvent", "0", PRIVATE);
+                }
+                break;
+        }
+            
 }
